6c9bc6018146aafdb97c440b358af5306b48713c
Karina <karina_karapetyan@mail.ru>
2022-08-09 12:21:21 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  // Указываем путь до входной точки:
+  entry: "./src/main.js",
+  // Описываем, куда следует поместить результат работы:
+  output: {
+    // Имя файла со сборкой:
+    filename: "bundle.js",
+    // Путь до директории (важно использовать path.resolve):
+    path: path.resolve(__dirname, "build"),
+    // Очистка директории для сборки перед новой сборкой:
+    clean: true,
+  },
+  // Генерация source-maps:
+  devtool: "source-map",
+  // Конфиг плагинов
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
