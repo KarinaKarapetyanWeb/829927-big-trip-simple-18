afbf135174ea699522b03b58758809e2254b55db
Karina <karina_karapetyan@mail.ru>
2022-09-20 22:47:04 +0300

    fix lint

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -5,8 +5,8 @@ const renderFilterOptionsTemplate = (filters, currentFilterType) =>
     .map(
       (filter) => `<div class="trip-filters__filter">
       <input id="filter-${filter.name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.type}" ${filter.count === 0 ? 'disabled' : ''} ${
-        filter.type === currentFilterType ? 'checked' : ''
-      }>
+  filter.type === currentFilterType ? 'checked' : ''
+}>
       <label class="trip-filters__filter-label" for="filter-${filter.name}">${filter.name}</label>
     </div>`
     )
